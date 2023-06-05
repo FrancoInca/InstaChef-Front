f22720a3946d3f581693d3640c67108872351e14
evinlopez90 <evinlopez90@gmail.com>
2023-06-05 16:46:16 -0500

    Detail

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+
 import './App.css'
 import { Route, Routes } from 'react-router-dom';
 
@@ -7,17 +8,18 @@ import NavBar from './components/NavBar';
 
 import Home from "./Componentes/Home/Home"
 import Menus from "./Componentes/Menus/Menus"
+import Detail from './Componentes/Detail/Detail';
 
 function App() {
   return (
     <div className="">
 
-     
+      <NavBar />
       <Routes>
-        <Route exact path='/' element={<NavBar />}/>
         <Route exact path="/" element={<LandingPage />} />
         <Route exact path="/Home" element={<Home/>} />
         <Route exact path="/Menus" element={<Menus/>} />
+        <Route exact path="/Detail" element={<Detail/>} />
       </Routes>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
  
-  purge: ['./index.html', "./src/Componentes", ".src/views/*",'./src/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: ['./index.html', "./src/Componentes", ".src/components", ".src/views/*",'./src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
