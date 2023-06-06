67e03aa02e2ea9fc65278f883c2df5ed6e52d474
Franco Inca <francoinca1604@gmail.com>
2023-06-06 17:46:09 -0500

    fix(Card): Fixed path in navigate

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,18 +4,18 @@ import { useNavigate } from "react-router-dom"
 function Card({ name, price, type, size, id }) {
   const navigate = useNavigate()
   return (
-    <div className="flex flex-col max-h-[120px] aspect-video m-4 hover:cursor-pointer" onClick={()=>{navigate(`detail/${id}`)}}>
+    <div className="flex flex-col max-h-[120px] aspect-video m-4 hover:cursor-pointer" onClick={() => { navigate(`/detail/${id}`) }}>
       <div>
         <img src="https://i.imgur.com/zJucG8M.png" alt={name} className="w-[100%] aspect-video" />
       </div>
       <div className="grid grid-cols-2">
         <div>
-        <p className="break-words text-left">{name}</p>
-        <p className="break-words text-left font-bold">{type}</p>
+          <p className="break-words text-left">{name}</p>
+          <p className="break-words text-left font-bold">{type}</p>
         </div>
         <div>
-        <p className="text-right">{size}</p>
-        <p className="text-right">${price}</p>
+          <p className="text-right">{size}</p>
+          <p className="text-right">${price}</p>
         </div>
       </div>
     </div>
