6be90dc36639e6244762cd2610d9a915b025b69d
Franco Inca <francoinca1604@gmail.com>
2023-06-20 15:43:21 -0500

    feat(App): Added dashboard

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,114 +1,120 @@
 import {
-    GET_ALL_PRODUCTS,
-    GET_DETAIL,
-    SEARCH_BY_NAME,LOGIN, SIGNUP, AGREGAR_PAGO, TRAER_PRODUCT_PAGOS, CUENTA, REVIEW, COMENTARIO, EDIT_NAME, EDIT_FOTO,
-    GET_USER_BY_ID,
-    UPDATE_PROFILE
-
-
+  GET_ALL_PRODUCTS,
+  GET_DETAIL,
+  SEARCH_BY_NAME,
+  LOGIN,
+  SIGNUP,
+  AGREGAR_PAGO,
+  TRAER_PRODUCT_PAGOS,
+  CUENTA,
+  REVIEW,
+  COMENTARIO,
+  EDIT_NAME,
+  EDIT_FOTO,
+  GET_USER_BY_ID,
+  UPDATE_PROFILE,
+  BAN_USER
 } from './variables';
 
 const initialState = {
-    products: [],
-    filtered: [],
-    detail: [],
-    filters: [],
-    userData: null,
-    pagoData: null,
-    productPagos: null,
-    cuenta: {
-        General: true,
-        MisDatos: false,
-        MisPedidos: false
-    }
+  products: [],
+  filtered: [],
+  detail: [],
+  filters: [],
+  userData: null,
+  pagoData: null,
+  productPagos: null,
+  cuenta: {
+    General: true,
+    MisDatos: false,
+    MisPedidos: false,
+  },
 };
 
-
 const rootReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case GET_ALL_PRODUCTS:
-            return {
-                ...state,
-                products: action.payload,
-                filtered: action.payload,
-            };
+  switch (action.type) {
+    case GET_ALL_PRODUCTS:
+      return {
+        ...state,
+        products: action.payload,
+        filtered: action.payload,
+      };
 
-        case SEARCH_BY_NAME:
-            return {
-                ...state,
-                filtered: action.payload,
-            };
+    case SEARCH_BY_NAME:
+      return {
+        ...state,
+        filtered: action.payload,
+      };
 
-        case GET_DETAIL: {
-            return {
-                ...state,
-                detail: action.payload,
-            };
-        }
-        case SIGNUP: {
-            return {
-                ...state,
-                userData: action.payload
-            }
-        }
-        case LOGIN: {
-            return {
-                ...state,
-                userData: action.payload
-            }
-        }
-        case GET_USER_BY_ID:
-            return {
-                ...state,
-                userData: action.payload,
-            };
-        case UPDATE_PROFILE:
-            return {
-                ...state,
-                userData: action.payload,
-            }
-        case AGREGAR_PAGO: {
-            return {
-                ...state,
-                pagoData: action.payload
-            }
-        }
-
-        case TRAER_PRODUCT_PAGOS: {
-            return {
-                ...state,
-                productPagos: action.payload
-            }
-        }
-
-        case CUENTA: {
-            return {
-                ...state,
-                cuenta: action.payload
-            }
-        }
-
-         case REVIEW: {
-            return console.log(action.payload);
-            
-         }
-
-         case COMENTARIO: {
-            return console.log(action.payload);
-            
-         }
-         case EDIT_NAME: {
-            return console.log(action.payload);
-            
-         }
-         case EDIT_FOTO: {
-            return console.log(action.payload);
-            
-         }
-
-        default:
-            return state;
+    case GET_DETAIL: {
+      return {
+        ...state,
+        detail: action.payload,
+      };
     }
-}
+    case SIGNUP: {
+      return {
+        ...state,
+        userData: action.payload,
+      };
+    }
+    case LOGIN: {
+      return {
+        ...state,
+        userData: action.payload,
+      };
+    }
+    case GET_USER_BY_ID:
+      return {
+        ...state,
+        userData: action.payload,
+      };
+    case UPDATE_PROFILE:
+      return {
+        ...state,
+        userData: action.payload,
+      };
+    case AGREGAR_PAGO: {
+      return {
+        ...state,
+        pagoData: action.payload,
+      };
+    }
+
+    case TRAER_PRODUCT_PAGOS: {
+      return {
+        ...state,
+        productPagos: action.payload,
+      };
+    }
+
+    case CUENTA: {
+      return {
+        ...state,
+        cuenta: action.payload,
+      };
+    }
+
+    case REVIEW: {
+      return console.log(action.payload);
+    }
+
+    case COMENTARIO: {
+      return console.log(action.payload);
+    }
+    case EDIT_NAME: {
+      return console.log(action.payload);
+    }
+    case EDIT_FOTO: {
+      return console.log(action.payload);
+    }
+    case BAN_USER: {
+      return console.log(action.payload)
+    }
+
+    default:
+      return state;
+  }
+};
 
 export default rootReducer;
--- src/redux/variables.js
+++ src/redux/variables.js
@@ -11,6 +11,7 @@ export const EDIT_FOTO = "EDIT_FOTO";
 export const EDIT_NAME = "EDIT_NAME";
 export const REVIEW = "REVIEW";
 export const COMENTARIO = "COMENTARIO";
+export const BAN_USER = 'BAN_USER';
 
 export const UPDATE_PROFILE = 'UPDATE_PROFILE';
 
