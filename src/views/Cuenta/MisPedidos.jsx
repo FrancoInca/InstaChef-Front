30758d3e4de21428417ba07bd6bf5bf80bf5a02d
evinlopez90 <evinlopez90@gmail.com>
2023-06-22 01:21:14 -0500

    recuperacion data

--- src/views/Cuenta/MisPedidos.jsx
+++ src/views/Cuenta/MisPedidos.jsx
@@ -1,47 +1,40 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom"
-import { BiCommentDetail } from "react-icons/bi"
-import { MdOutlineReviews } from "react-icons/md";
+import { Link } from "react-router-dom";
+import { BiCommentDetail } from "react-icons/bi";
+// import { MdOutlineReviews } from "react-icons/md";
 import { AiFillStar, AiOutlineStar } from "react-icons/ai";
-import {setComentario} from '../../redux/actions';
+import { setComentario } from "../../redux/actions";
 export default function MisPedidos() {
   let dispatch = useDispatch();
   let productPagos = useSelector((state) => state.productPagos);
-  const [comentarios, setComentarios] = useState('');
+  const [comentarios, setComentarios] = useState("");
   const [reviewState, setReviewState] = useState(2);
   const [message, setMessage] = useState(null);
   const [popUp, setPopUp] = useState(null);
-  const [popUpReview, setPopUpReview] = useState(null);
-  const [idReview, setIdReview] = useState(null);
+  // const [popUpReview, setPopUpReview] = useState(null);
+  const [error, setError] = useState(null);
   const [idComment, setIdComment] = useState(null);
 
-  let token = localStorage.getItem('token');
+  let token = localStorage.getItem("token");
   const reset = () => {
-    if (comentarios.length > 1) {
-      console.log(idComment, comentarios);
+    if (comentarios.length > 1 && reviewState > 0) {
+      console.log(idComment, comentarios, reviewState);
+      setError(null);
       dispatch(
         setComentario({
-          id: idComment,
-          comentarios: comentarios,
+          productId: idComment,
+          body: comentarios,
           token,
+          rating: reviewState,
         })
       );
       setMessage(true);
+    } else {
+      setError(true);
     }
   };
 
-  const resetReview = () => {
-    if (reviewState > 0) {
-      console.log(idReview, reviewState);
-      // dispatch(setReview({
-      //   id: idReview,
-      //   reviewState: reviewState,
-      //   token
-      // }))
-      setMessage(true);
-    }
-  };
   return (
     <div className=" flex justify-center ml-32">
       {popUp === true ? (
@@ -49,8 +42,10 @@ export default function MisPedidos() {
           <button
             onClick={() => {
               setPopUp(false);
-              setComentarios('');
+              setComentarios("");
               setMessage(null);
+              setReviewState(0);
+              setError(null);
             }}
             className="absolute -end-1 -top-1 rounded-full  bg-red-600 p-1"
           >
@@ -70,18 +65,44 @@ export default function MisPedidos() {
           </button>
 
           <div className="flex flex-col items-center justify-center gap-4 p-4">
-            <div className="h-20 w-4/5  ">
+            <div className="h-32 w-4/5  ">
               {message === true ? (
                 <p className="text-[13px] text-center flex items-center h-20 justify-center text-gray-600">
                   Gracias por tu comentario
                 </p>
               ) : (
-                <input
-                  type="text"
-                  onChange={(e) => setComentarios(e.target.value)}
-                  placeholder="Escribe tu comentario aquí"
-                  className=" text-black w-full h-14 py-5 px-3 bg-transparent border border-amber-400 rounded-[7px] outline-none placeholder:text-gray-500 "
-                />
+                <>
+                  <input
+                    type="text"
+                    onChange={(e) => setComentarios(e.target.value)}
+                    placeholder="Escribe tu comentario aquí"
+                    className=" text-black w-full h-14 py-5 px-3 bg-transparent border border-amber-400 rounded-[7px] outline-none placeholder:text-gray-500 "
+                  />
+                  <div className="flex text-amber-400 m-5 text-[30px] cursor-pointer">
+                    <h1 className="text-gray-600 text-[15px] flex justify-center items-center ">
+                      Calificación:{" "}
+                    </h1>
+                    {[...new Array(5)].map((start, index) => {
+                      return index < reviewState ? (
+                        <AiFillStar
+                          onClick={() => setReviewState(index)}
+                          key={index}
+                        />
+                      ) : (
+                        <AiOutlineStar
+                          onClick={() => setReviewState(index + 1)}
+                          key={index}
+                        />
+                      );
+                    })}
+                  </div>
+                  {error === true ? (
+                    <p className="text-[11px] text-red-800">
+                      {" "}
+                      has un comentario y da una puntuación
+                    </p>
+                  ) : null}
+                </>
               )}
             </div>
             <div className="flex justify-center">
@@ -99,68 +120,6 @@ export default function MisPedidos() {
         </form>
       ) : null}
 
-      {popUpReview === true ? (
-        <div className=" fixed bg-white z-50  rounded-lg border border-gray-200 shadow-lg">
-          <button
-            onClick={() => {
-              setPopUpReview(null);
-              setReviewState();
-              setMessage(null);
-            }}
-            className="absolute -end-1 -top-1 rounded-full  bg-red-600 p-1"
-          >
-            <span className="sr-only">Close</span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-3 w-3"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </button>
-
-          {message === null ? (
-            <>
-              <div className="flex text-amber-400 m-20 text-[20px] cursor-pointer">
-                <h1 className="text-gray-600 text-[12px] flex justify-center items-center ">
-                  Calificación:{' '}
-                </h1>
-                {[...new Array(5)].map((start, index) => {
-                  return index < reviewState ? (
-                    <AiFillStar
-                      onClick={() => setReviewState(index)}
-                      key={index}
-                    />
-                  ) : (
-                    <AiOutlineStar
-                      onClick={() => setReviewState(index + 1)}
-                      key={index}
-                    />
-                  );
-                })}
-              </div>
-
-              <div className="flex justify-center items-center m-10">
-                <button
-                  onClick={() => resetReview()}
-                  className="w-56 rounded-md h-8 border-none shadow-md hover:border bg-amber-400 hover:border-amber-400 hover:bg-transparent hover:text-black"
-                >
-                  Guardar
-                </button>
-              </div>
-            </>
-          ) : (
-            <p className="text-[13px] text-center flex items-center h-20 justify-center text-gray-600 m-20">
-              gracias por calificar este plato
-            </p>
-          )}
-        </div>
-      ) : null}
       <section>
         <div className="mx-auto  px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
           <div className="mx-auto max-w-3xl">
@@ -211,27 +170,13 @@ export default function MisPedidos() {
                             Comentar
                           </button>
                         </div>
-                        <div>
-                          <button
-                            onClick={() => {
-                              setPopUpReview(true);
-                              setIdReview(p.id);
-                            }}
-                            className="text-[11px] text-gray-400 "
-                          >
-                            <span>
-                              <MdOutlineReviews />
-                            </span>
-                            Review
-                          </button>
-                        </div>
                       </div>
                     </li>
                   ))
                 ) : (
                   <div>
                     <h1 className="font-bold text-[16px]">
-                      {' '}
+                      {" "}
                       No has hecho ningún pedido.
                     </h1>
                     <p className="text-[13px]">
@@ -244,8 +189,6 @@ export default function MisPedidos() {
 
               <div className="mt-8 flex justify-end border-t border-gray-100 pt-8">
                 <div className="w-screen max-w-lg space-y-4">
-                  <div className="flex justify-end"></div>
-
                   <div className="flex justify-end">
                     <Link
                       to="/Home"
